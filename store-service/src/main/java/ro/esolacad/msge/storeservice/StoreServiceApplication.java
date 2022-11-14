2ff7e381a23259e158ba830cd2cfeb51ad5ce759
Calin Nicoara <calin.nicoara@esolutions.ro>
2022-11-14 15:46:37 +0200

    Using discovery service on Feing Client and api-gateway

--- store-service/src/main/java/ro/esolacad/msge/storeservice/StoreServiceApplication.java
+++ store-service/src/main/java/ro/esolacad/msge/storeservice/StoreServiceApplication.java
@@ -2,10 +2,12 @@ package ro.esolacad.msge.storeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableDiscoveryClient
 public class StoreServiceApplication {
 
 	public static void main(String[] args) {
--- store-service/src/main/java/ro/esolacad/msge/storeservice/product/feign/InventoryFeignClient.java
+++ store-service/src/main/java/ro/esolacad/msge/storeservice/product/feign/InventoryFeignClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import ro.esolacad.msge.storeservice.product.ProductInventoryModel;
 
 @FeignClient(name="inventory-service",
-        url = "http://localhost:8082",
+//        url = "http://localhost:8082",
 //        fallback = InventoryFeignClientFallback.class,
         fallbackFactory = InventoryFeignClientFallbackFactory.class
 )

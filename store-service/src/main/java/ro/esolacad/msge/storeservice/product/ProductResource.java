b999a7267e4362230c8a9d3b8c65e3da7b798c59
Calin Nicoara <calin.nicoara@esolutions.ro>
2022-11-14 11:51:04 +0200

    Added get product code method on ProductResource

--- store-service/src/main/java/ro/esolacad/msge/storeservice/product/ProductResource.java
+++ store-service/src/main/java/ro/esolacad/msge/storeservice/product/ProductResource.java
@@ -2,10 +2,12 @@ package ro.esolacad.msge.storeservice.product;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RequestMapping("/products")
 @RestController
@@ -13,9 +15,15 @@ import java.util.List;
 public class ProductResource {
 
     private final ProductRepository productRepository;
+    private final ProductService productService;
 
     @GetMapping()
     public List<Product> getProducts() {
         return productRepository.findAll();
+    }
+
+    @GetMapping("/{product_code}")
+    public Optional<ProductModel> getProduct(@PathVariable("product_code") String code) {
+        return productService.findByProductCode(code);
     }
 }
